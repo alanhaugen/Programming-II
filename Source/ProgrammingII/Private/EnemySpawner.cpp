ca57d8499b60556dd71807b2565ed0fdfdd83731
Thomas269415 <269415@stud.inn.no>
2024-04-03 14:27:32 +0200

    Added spawning

---
+++ Source/ProgrammingII/Private/EnemySpawner.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawner.h"
+#include "Math/UnrealMathUtility.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "Enemy.h"
+
+// Sets default values
+AEnemySpawner::AEnemySpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+
+	SpawnTime = 3.f;
+
+	if (RunningTime > SpawnTime) 
+	{
+		RunningTime = 0.f;
+
+		GetWorld()->SpawnActor<AEnemy>(EnemyClass, GetActorLocation(), FRotator::ZeroRotator);
+	}
+
+}
+
