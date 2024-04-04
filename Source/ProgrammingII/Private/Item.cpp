dfd1b0877a6436dba904f6dc24d43ea75892d2fa
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2024-04-04 13:03:32 +0200

    Items will now be destroyed on overlap

--- Source/ProgrammingII/Private/Item.cpp
+++ Source/ProgrammingII/Private/Item.cpp
@@ -1,24 +1,42 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Item.h"
+#include "Components/BoxComponent.h"
 
-// Sets default values
 AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CollisionMesh = CreateDefaultSubobject<UBoxComponent>(FName("Collision Mesh"));
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	CollisionMesh->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSpehereOverlap);
+	CollisionMesh->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSpehereEndOverlap);
 }
 
-// Called every frame
+void AItem::OnSpehereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Overlap item"));
+	}
+
+	Destroy();
+}
+
+void AItem::OnSpehereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("End overlap item"));
+	}
+}
+
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/ProgrammingII/Public/Item.h
+++ Source/ProgrammingII/Public/Item.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class PROGRAMMINGII_API AItem : public AActor
 {
@@ -17,6 +19,15 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// Collision mesh for item
+	UBoxComponent* CollisionMesh;
+
+	UFUNCTION()
+	virtual void OnSpehereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnSpehereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 public:	
 	// Called every frame
