30234436683066f8f3105a7e723c1baac6fc1e74
Thomas269415 <269415@stud.inn.no>
2024-04-06 17:49:16 +0200

    Line trace only when aiming

--- Source/ProgrammingII/Private/Harker.cpp
+++ Source/ProgrammingII/Private/Harker.cpp
@@ -237,36 +237,41 @@ void AHarker::Tick(float DeltaTime)
 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, f);
 	}
 	*/
+
 	
-	// FHitResult will hold all data returned by our line collision query
-	FHitResult Hit;
-
-	// We set up a line trace from our current location to a point 1000cm ahead of us
-	FVector TraceStart = GetActorLocation();
-	FVector TraceEnd = GetActorLocation() + GetActorForwardVector() * 5000.0f;
-
-	// You can use FCollisionQueryParams to further configure the query
-	// Here we add ourselves to the ignored list so we won't block the trace
-	FCollisionQueryParams QueryParams;
-	QueryParams.AddIgnoredActor(this);
-
-	// To run the query, you need a pointer to the current level, which you can get from an Actor with GetWorld()
-	// UWorld()->LineTraceSingleByChannel runs a line trace and returns the first actor hit over the provided collision channel.
-	GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, TraceChannelProperty, QueryParams);
-
-	// You can use DrawDebug helpers and the log to help visualize and debug your trace queries.
-	DrawDebugLine(GetWorld(), TraceStart, TraceEnd, Hit.bBlockingHit ? FColor::Blue : FColor::Red, false, 5.0f, 0, 10.0f);
-	UE_LOG(LogTemp, Log, TEXT("Tracing line: %s to %s"), *TraceStart.ToCompactString(), *TraceEnd.ToCompactString());
-
-	// If the trace hit something, bBlockingHit will be true,
-	// and its fields will be filled with detailed info about what was hit
-	if (Hit.bBlockingHit && IsValid(Hit.GetActor()))
+	if (isZoomingIn) 
 	{
-		UE_LOG(LogTemp, Log, TEXT("Trace hit actor: %s"), *Hit.GetActor()->GetName());
+		// FHitResult will hold all data returned by our line collision query
+		FHitResult Hit;
+
+		// We set up a line trace from our current location to a point 1000cm ahead of us
+		FVector TraceStart = GetActorLocation();
+		FVector TraceEnd = GetActorLocation() + GetActorForwardVector() * 5000.0f;
+
+		// You can use FCollisionQueryParams to further configure the query
+		// Here we add ourselves to the ignored list so we won't block the trace
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(this);
+
+		// To run the query, you need a pointer to the current level, which you can get from an Actor with GetWorld()
+		// UWorld()->LineTraceSingleByChannel runs a line trace and returns the first actor hit over the provided collision channel.
+		GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, TraceChannelProperty, QueryParams);
+
+		// You can use DrawDebug helpers and the log to help visualize and debug your trace queries.
+		DrawDebugLine(GetWorld(), TraceStart, TraceEnd, Hit.bBlockingHit ? FColor::Blue : FColor::Red, false, 5.0f, 0, 10.0f);
+		UE_LOG(LogTemp, Log, TEXT("Tracing line: %s to %s"), *TraceStart.ToCompactString(), *TraceEnd.ToCompactString());
+
+		// If the trace hit something, bBlockingHit will be true,
+		// and its fields will be filled with detailed info about what was hit
+		if (Hit.bBlockingHit && IsValid(Hit.GetActor()))
+		{
+			UE_LOG(LogTemp, Log, TEXT("Trace hit actor: %s"), *Hit.GetActor()->GetName());
+		}
+		else {
+			UE_LOG(LogTemp, Log, TEXT("No Actors were hit"));
+		}
 	}
-	else {
-		UE_LOG(LogTemp, Log, TEXT("No Actors were hit"));
-	}
+	
 
 }
 
