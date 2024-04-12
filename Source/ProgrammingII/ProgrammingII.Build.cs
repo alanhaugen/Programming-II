e3815c14962ade5d07bbf15cea304dc8b1b9a2bc
Hans Alan Whitburn Haugen <alanhaugen@gmail.com>
2024-04-13 00:01:28 +0200

    Changed build cs file a little for esthetics

--- Source/ProgrammingII/ProgrammingII.Build.cs
+++ Source/ProgrammingII/ProgrammingII.Build.cs
@@ -8,7 +8,17 @@ public class ProgrammingII : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimGraphRuntime", "AIModule", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AnimGraphRuntime",
+			"AIModule",
+			"UMG"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
